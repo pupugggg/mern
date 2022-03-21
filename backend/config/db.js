d9a0b437f46e25a8b92e6faf1bb92a1023619734
pupugggg <pupugggg@gmail.com>
2022-03-21 18:12:21 +0800

    add  option for db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose')
 //define how to connect to MongoDB.
 const connectDB = async () => {
     try {
-        console.log(process.env.MONGO_URI)
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          })
         
         console.log(`MongoDB Connected ${conn.connection.host}`.cyan.underline)
     } catch(error) {
