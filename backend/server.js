c4807e74690cb18ce984f95e733f3eef2df4ee21
pupugggg <pupugggg@gmail.com>
2022-03-21 15:55:06 +0800

    prepare to be deployed

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,15 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const cors = require('cors')
+const { sendfile } = require('express/lib/response')
 const port = process.env.port || 5000
 connectDB()
 //  initialize server.
 const app = express()
-//  allow cors
-const corsWhitlist = ['http://localhost:3000']
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//       if (!origin || whitelist.indexOf(origin) !== -1) {
-//         callback(null, true)
-//       } else {
-//         callback(new Error("Not allowed by CORS"))
-//       }
-//     },
-//     credentials: true,
-//   }
-// app.use(cors(corsOptions))
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
@@ -36,6 +24,14 @@ app.use(express.urlencoded({ extended: false }))
 //  Set the route of the server
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+if(process.env.NODE_ENV=="production"){
+  app.use(express.static(path.join(__dirname,'../frontend/build')))
+  app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'../','frontend','build','index.html')))
+}else{
+  app.get('/',(req,res)=>res.send('please set to production'))
+}
+
 
 //  Overwrite the default error handler so that server won't respond a html file to show the error.
 app.use(errorHandler)
