a87fe239d9767e8d090a8cf74e9677d0585cfa0f
pupugggg <pupugggg@gmail.com>
2022-03-21 17:39:03 +0800

    cidi

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 //define how to connect to MongoDB.
 const connectDB = async () => {
     try {
+        console.log(process.env.MONGO_URI)
         const conn = await mongoose.connect(process.env.MONGO_URI)
+        
         console.log(`MongoDB Connected ${conn.connection.host}`.cyan.underline)
     } catch(error) {
         console.log(error)
